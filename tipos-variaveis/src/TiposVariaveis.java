37040fb45d5725ff51263339b7f5098a3b635d8a
lacomb04 <victorlacombdemelo@hotmail.com>
2024-09-04 00:35:40 -0300

    atualização

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,8 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         String meuNome ="Victor Lacomb";
-        int numeroCasa = 993;
+        int idade = 24;
         double salario = 1940.40;
-        
+        boolean Empregado = true;
+        char sexo = 'M';
+
     }
 }
