3073229b9c369f6b6c930b3a9d181807c4446e70
lacomb04 <victorlacombdemelo@hotmail.com>
2024-09-07 19:58:07 -0300

    update Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a,b;
+        a = 1;
+        b=1;
+        String resultado = a==b ?"sim": "não";
+        System.out.println(resultado);
+    }
+}
